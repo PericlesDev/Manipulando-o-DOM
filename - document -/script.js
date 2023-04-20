c4e920b82bbdadaa9624e0d273b1e1f6f611f043
unknown <PericlesDosSantosRamos@hotmail.com>
2023-04-20 15:40:38 -0300

    Manipulando o HTML5 com Javascript | document

---
+++ - document -/script.js
@@ -0,0 +1,10 @@
+//alterando o título
+document.title = "Javascript - DOM alterado";
+
+//alterando cor de fundo
+document.body.style.background = "#ccc";
+
+//alterando imagem
+document.body.style.backgroundImage = "url('./image/Sem título.png')"
+
+
